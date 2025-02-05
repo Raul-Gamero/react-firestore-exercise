ec9b3312011a8798c780807129c6c2b13b161837
Raul Gamero <raul.a.gamero@gmail.com>
2025-02-05 11:38:16 +0100

    updating async functions

--- my-songs-app/src/components/SongList.jsx
+++ my-songs-app/src/components/SongList.jsx
@@ -1,22 +1,20 @@
 import { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
-import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { collection, onSnapshot, deleteDoc, doc } from "firebase/firestore";
 import SongItem from "./SongItem";
 
 const SongList = () => {
   const [songs, setSongs] = useState([]);
 
   useEffect(() => {
-    const fetchSongs = async () => {
-      const querySnapshot = await getDocs(collection(db, "Songs"));
-      setSongs(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
-    };
-    fetchSongs();
+    const unsubscribe = onSnapshot(collection(db, "Songs"), (snapshot) => {
+      setSongs(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+    });
+    return () => unsubscribe();
   }, []);
 
   const deleteSong = async (id) => {
     await deleteDoc(doc(db, "Songs", id));
-    setSongs(songs.filter(song => song.id !== id));
   };
 
   return (
