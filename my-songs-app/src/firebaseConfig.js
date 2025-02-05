b91018ec4dd5a88d621aa202e2472db64dc2baf9
Raul Gamero <raul.a.gamero@gmail.com>
2025-02-05 12:05:01 +0100

    DONE

--- my-songs-app/src/firebaseConfig.js
+++ my-songs-app/src/firebaseConfig.js
@@ -5,9 +5,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyDk-Ba3_yHSVD8-jZR5IvysGL__OJRKN7A",
   authDomain: "my-songs-app.firebaseapp.comMAIN",
   projectId: "my-songs-app-c436e",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
